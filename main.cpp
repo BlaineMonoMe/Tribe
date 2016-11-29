b643467713fe8366d46d5f18208557740ba17a52
BlaineMonoMe <fallower@ukr.net>
2016-11-29 12:10:09 +0200

    constants defined out of main function

--- main.cpp
+++ main.cpp
@@ -1,31 +1,35 @@
 #include <SDL.h>
 #include <stdio.h>
 
-//The window we'll be rendering to
-SDL_Window* window = NULL;
 
-//The surface contained by the window
-SDL_Surface* screenSurface = NULL;
+const int SCREEN_WIDTH = 1366;
+const int SCREEN_HEIGHT = 768;
+const char *PROJECT_NAME = "Tibe";
+
+SDL_Window *window = NULL;
+SDL_Surface *screen = NULL;
 
 
 int main(int argc, char** args)
 {
 
     bool quit = false;
-    SDL_Event e;
+
+    SDL_Event event;
 
     SDL_Init(SDL_INIT_VIDEO);
-    window = SDL_CreateWindow("Tribal", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1366, 768, SDL_WINDOW_FULLSCREEN);
-    screenSurface = SDL_GetWindowSurface(window);
+    window = SDL_CreateWindow(PROJECT_NAME, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                              SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN);
+    screen = SDL_GetWindowSurface(window);
 
 
     while(quit == false)
     {
-        while(SDL_PollEvent(&e) != 0)
+        while(SDL_PollEvent(&event) != 0)
         {
-            if(e.type == SDL_KEYDOWN)
+            if(event.type == SDL_KEYDOWN)
             {
-                if(e.key.keysym.sym = SDLK_ESCAPE)
+                if(event.key.keysym.sym = SDLK_ESCAPE)
                 {
                     quit = true;
                 }
@@ -34,8 +38,8 @@ int main(int argc, char** args)
     }
 
 
-	SDL_FreeSurface(screenSurface);
-	screenSurface = NULL;
+	SDL_FreeSurface(screen);
+	screen = NULL;
 
 	SDL_DestroyWindow(window);
 	window = NULL;
